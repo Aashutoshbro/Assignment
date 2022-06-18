da5a09e7cb534edcaca15230615ea95a257ab6cd
Aashutosh Sapkota <55585284+Aashutoshbro@users.noreply.github.com>
2022-06-18 21:30:13 +0545

    Add files via upload

---
+++ Assignment_first/assign_03.cpp
@@ -0,0 +1,45 @@
+/*Program 3: Using a class write a program that receives inputs, principle amount, time and rate. Keeping rate 8% as the default argument, 
+calculate the simple interest for three customers.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class customer{
+	float principle;
+	int time;
+	int rate;
+	
+	public:
+		void get_Data(){
+			cout<<"Enter Principle amount: "<<endl;
+			cin>>principle;
+			cout<<"Enter Time period: "<<endl;
+			cin>>time;
+			cout<<"We have rate 0f 8% for all customers:"<<endl;
+		}
+		
+		void display(int r=8){
+			rate=r;
+			float Interest;
+			Interest = (principle*time*rate)/100;
+			cout<<"The Simple interest of our kind customer is: "<<Interest<<endl<<endl;
+			
+		}
+};
+
+
+int main(){
+	
+	customer a, b, c;
+	cout<<"..............Enter the following..................."<<endl<<endl;
+	a.get_Data();
+	a.display();
+	b.get_Data();
+	b.display();
+	c.get_Data();
+	c.display();
+	
+	return 0;
+
+}
+
